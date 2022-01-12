62ce89ad88a7d77d4159162c4d103e93572696e1
Paul Bailey <paul@neutron.studio>
2022-01-12 11:52:40 -0600

    logging

--- shelix/settings/production.py
+++ shelix/settings/production.py
@@ -1,1 +1,31 @@
 from shelix.settings.base import *
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{asctime}] [{levelname}] [{name}] {message}',
+            'style': '{',
+            'datefmt': "%Y-%m-%dT%H:%M:%S %z"
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        }
+    }
+}
